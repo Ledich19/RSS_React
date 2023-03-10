7cc84bac2e3b97dd60e02650397cc04bc7e62fcb
Aleksander Chumachenko <ledich19@gmail.com>
2023-03-10 17:19:52 +0200

    feat: add search component

---
+++ ReactComponents/src/components/Search/Search.tsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import s from './Search.module.css';
+import { FaSearch } from 'react-icons/fa';
+
+interface Props {}
+
+interface State {
+  value: string;
+}
+
+export default class Search extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      value: localStorage.getItem('searchString') || '',
+    };
+  }
+
+  componentWillUnmount() {
+    const { value } = this.state;
+    console.log(`Value "${value}" was saved to LocalStorage for component `);
+    localStorage.setItem('searchString', value);
+  }
+
+  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ value: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className={s.wrap}>
+        <div className={s.search}>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleInputChange}
+            className={s.searchTerm}
+            placeholder="What are you looking for?"
+          />
+          <button type="submit" className={s.searchButton}>
+            <div className={s.icon}>
+              <FaSearch />
+            </div>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
