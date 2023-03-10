ea0ae8608a134273ab909be653812cd253fc0c39
Aleksander Chumachenko <ledich19@gmail.com>
2023-03-10 14:06:14 +0200

    feat: add react routers and  404 , about as pages

--- ReactComponents/src/App.tsx
+++ ReactComponents/src/App.tsx
@@ -1,6 +1,7 @@
 
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
+import { Outlet, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
--- ReactComponents/src/main.tsx
+++ ReactComponents/src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Page404 from './components/Page404'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import AboutUs from './components/AboutUs';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Page404/> ,
+  },
+  {
+    path: "/about",
+    element: <AboutUs />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
