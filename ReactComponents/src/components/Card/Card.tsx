8846d331b37aa67381d5f71eb0ec55c624c404ff
Aleksander Chumachenko <ledich19@gmail.com>
2023-03-11 08:50:56 +0200

    feat: add Colection component

--- ReactComponents/src/components/Card/Card.tsx
+++ ReactComponents/src/components/Card/Card.tsx
@@ -4,12 +4,12 @@ import s from './Card.module.css';
 interface Props {
   infoData: {
     title: string;
-    isbn: string;
+    isbn?: string;
     pageCount: number;
     publishedDate: { $date: string };
-    thumbnailUrl: string;
-    shortDescription: string;
-    longDescription: string;
+    thumbnailUrl?: string;
+    shortDescription?: string;
+    longDescription?: string;
     status: string;
     authors: string[];
     categories: string[];
@@ -40,31 +40,30 @@ export default class Card extends Component<Props, State> {
     return (
       <div className={s.card}>
         <h2 className={s.title}>{title}</h2>
-        <div className={s.box}>
-          <div className={s.img}>
-            <img src={thumbnailUrl} alt={title} />
-          </div>
-          <div className={s.info}>
-            <div className={s.pages}>
-              <span className={s.textBold}>Pages: </span>
-              {pageCount}
-            </div>
-            <div className={s.status}>
-              <span className={s.textBold}>Status: </span>
-              {status}
-            </div>
-            <div className={s.date}>
-              <span className={s.textBold}>Published:&nbsp;</span>
-              {new Date(publishedDate.$date).toLocaleDateString('ru-RU')}
-            </div>
-            <div className={s.categories}>
-              <span className={s.textBold}>Categories: </span>
-              <span className={s.categoriesList}>{categories.join(', ')}</span>
-            </div>
-          </div>
+        <div className={s.img}>
+          <img src={thumbnailUrl} alt={title} />
         </div>
         <div className={s.authors}>{authors.join(', ')}</div>
-        <div className={s.shortDescription}>{shortDescription}</div>
+        <div className={s.info}>
+          <div className={s.pages}>
+            <span className={s.textBold}>Pages: </span>
+            {pageCount}
+          </div>
+          <div className={s.status}>
+            <span className={s.textBold}>Status: </span>
+            {status}
+          </div>
+          <div className={s.date}>
+            <span className={s.textBold}>Published:&nbsp;</span>
+            {new Date(publishedDate.$date).toLocaleDateString('ru-RU')}
+          </div>
+          <div className={s.categories}>
+            <span className={s.textBold}>Categories: </span>
+            <span className={s.categoriesList}>{categories.join(', ')}</span>
+          </div>
+        </div>
+
+        {/* <div className={s.shortDescription}>{shortDescription}</div> */}
         {/* <p>Long description: {longDescription}</p> */}
       </div>
     );
