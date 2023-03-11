65ac8db51fbde5595725248d8ff93f2de1d15e6c
Aleksander Chumachenko <ledich19@gmail.com>
2023-03-11 13:23:42 +0200

    test: add test for Search

---
+++ ReactComponents/src/components/Search/SearchComponent.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen, fireEvent } from '@testing-library/react';
+import SearchComponent from './SearchComponent';
+import SearchContainer from './SearchContainer';
+import userEvent from '@testing-library/user-event';
+
+describe('<SearchComponent />', () => {
+  test('input should update value correctly', async () => {
+    render(<SearchContainer handleSearch={() => {}} />);
+    const input = screen.getByRole('textbox');
+    await userEvent.type(input, 'testing a form...');
+    expect(input).toHaveValue('testing a form...');
+  });
+
+  test('submit button should call handleSearch', () => {
+    const handleSearch = jest.fn();
+    render(
+      <SearchComponent
+        value={'search value test'}
+        handleInputChange={() => {}}
+        handleSearch={handleSearch}
+      />
+    );
+
+    const searchButton = screen.getByRole('button');
+    fireEvent.click(searchButton);
+
+    expect(handleSearch).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('<SearchContainer />', () => {
+  test('submit button should call setSearchState', () => {
+    const setSearchState = jest.fn();
+    render(<SearchContainer setSearchState={setSearchState} />);
+
+    const input = screen.getByRole('textbox');
+    fireEvent.change(input, { target: { value: 'testing a form...' } });
+
+    const searchButton = screen.getByRole('button');
+    fireEvent.click(searchButton);
+
+    expect(setSearchState).toHaveBeenCalledTimes(1);
+    expect(setSearchState.mock.calls[0][0]).toBe('testing a form...');
+  });
+});
