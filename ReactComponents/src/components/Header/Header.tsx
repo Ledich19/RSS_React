5d11857d8c1841fe769f52c77a3b1939d2bae258
Aleksander Chumachenko <ledich19@gmail.com>
2023-03-19 10:30:21 +0200

    feat add hidden searc element for enother pages:

--- ReactComponents/src/components/Header/Header.tsx
+++ ReactComponents/src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import s from './Header.module.scss';
 import { Link } from 'react-router-dom';
 import SearchContainer from '../Search/SearchContainer';
+import { Outlet, Routes, Route } from 'react-router-dom';
 
 interface Props {
   setSearchState: (value: string) => void;
@@ -15,10 +16,17 @@ export default class Header extends Component<Props> {
           <Link to={`/`}>Home</Link>
           <Link to={`about`}>About us</Link>
         </div>
-        <SearchContainer
-          data-testid="search-container"
-          setSearchState={this.props.setSearchState}
-        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <SearchContainer
+                data-testid="search-container"
+                setSearchState={this.props.setSearchState}
+              />
+            }
+          />
+        </Routes>
       </div>
     );
   }
