8fa4abd52fd5553d67e1d49d77a3608835453deb
Aleksandr <ledich19@gmail.com>
2023-03-20 13:39:44 +0200

    style: add darck becground

---
+++ ReactComponents/vite.config.js
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './setupTest.ts',
+    },
+    // optimizeDeps: {
+    //   include: [
+    //     'src/**/*.ts',
+    //     'src/**/*.tsx',
+    //     'src/**/*.js',
+    //     'src/**/*.jsx',
+    //     'public/index.html',
+    //     'src/**/*.css',
+    //     'src/**/*.scss',
+    //   ],
+    // },
+});
