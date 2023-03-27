7b6dcc61288feeca8ad55c7b2ac6aea20f5c8a52
Aleksandr <ledich19@gmail.com>
2023-03-27 21:20:23 +0300

    refactor: App into function component

--- ReactComponents/src/App.tsx
+++ ReactComponents/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { Outlet, Routes, Route, Navigate } from 'react-router-dom';
 import { InfoData } from 'app/types';
 import AboutUs from './pages/AboutUs/AboutUs';
@@ -9,42 +9,29 @@ import Collection from './pages/Collection/Collection';
 import booksData from './data/booksDb.json';
 import AddBook from './pages/AddBook/AddBook';
 
-interface AppState {
-  search: string;
-  books: InfoData[];
-}
+function App() {
+  const [search, setSearch] = useState('');
+  const [books] = useState(booksData);
 
-class App extends Component<object, AppState> {
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      search: '',
-      books: booksData,
-    };
-  }
-
-  setSearchState = (value: string) => {
-    this.setState({ search: value });
+  const setSearchState = (value: string) => {
+    setSearch(value);
   };
 
-  render() {
-    const { search } = this.state;
-    return (
-      <div data-testid="App-testId" className="App">
-        <Routes>
-          <Route path="/" element={<Layout setSearchState={this.setSearchState} />}>
-            <Route index element={<Navigate to="/app" />} />
-            <Route path="app" element={<Collection books={this.state.books} search={search} />} />
-            <Route path="about" element={<AboutUs />} />
-            <Route path="blank" element={<AddBook />} />
-            <Route path="404" element={<Page404 />} />
-            <Route path="*" element={<Navigate to="404" />} />
-          </Route>
-        </Routes>
-        <Outlet />
-      </div>
-    );
-  }
+  return (
+    <div data-testid="App-testId" className="App">
+      <Routes>
+        <Route path="/" element={<Layout setSearchState={setSearchState} />}>
+          <Route index element={<Navigate to="/app" />} />
+          <Route path="app" element={<Collection books={books} search={search} />} />
+          <Route path="about" element={<AboutUs />} />
+          <Route path="blank" element={<AddBook />} />
+          <Route path="404" element={<Page404 />} />
+          <Route path="*" element={<Navigate to="404" />} />
+        </Route>
+      </Routes>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
