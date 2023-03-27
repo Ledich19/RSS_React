5ec4eb7a19fdd76bcb45bdded2cab2b3c7ac646f
Aleksandr <ledich19@gmail.com>
2023-03-27 18:28:31 +0300

    refactor: Header  to function component

--- ReactComponents/src/components/Header/Header.tsx
+++ ReactComponents/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { NavLink, Routes, Route } from 'react-router-dom';
 import s from './Header.module.scss';
 import SearchContainer from '../Search/SearchContainer';
@@ -7,32 +7,30 @@ interface Props {
   setSearchState: (value: string) => void;
 }
 
-export default class Header extends PureComponent<Props> {
-  render() {
-    const { setSearchState } = this.props;
-
-    return (
-      <div className={s.header}>
-        <div className={s.list}>
-          <NavLink to="/app" className={(info) => (info.isActive ? s.activeLink : s.navLink)}>
-            Home
-          </NavLink>
-          <NavLink to="/about" className={(info) => (info.isActive ? s.activeLink : s.navLink)}>
-            About us
-          </NavLink>
-          <NavLink to="/blank" className={(info) => (info.isActive ? s.activeLink : s.navLink)}>
-            Add book
-          </NavLink>
-        </div>
-        <Routes>
-          <Route
-            path="/app"
-            element={
-              <SearchContainer data-testid="search-container" setSearchState={setSearchState} />
-            }
-          />
-        </Routes>
+const Header: React.FC<Props> = ({ setSearchState }) => {
+  return (
+    <div className={s.header}>
+      <div className={s.list}>
+        <NavLink to="/app" className={(info) => (info.isActive ? s.activeLink : s.navLink)}>
+          Home
+        </NavLink>
+        <NavLink to="/about" className={(info) => (info.isActive ? s.activeLink : s.navLink)}>
+          About us
+        </NavLink>
+        <NavLink to="/blank" className={(info) => (info.isActive ? s.activeLink : s.navLink)}>
+          Add book
+        </NavLink>
       </div>
-    );
-  }
-}
+      <Routes>
+        <Route
+          path="/app"
+          element={
+            <SearchContainer data-testid="search-container" setSearchState={setSearchState} />
+          }
+        />
+      </Routes>
+    </div>
+  );
+};
+
+export default Header;
