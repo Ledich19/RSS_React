4952879fe92c4d6425ebb38d2fa705da98f40cbb
Aleksandr <ledich19@gmail.com>
2023-03-27 18:30:36 +0300

    refactor: Notify to function component

--- ReactComponents/src/components/NotifyComponent/NotifyComponent.tsx
+++ ReactComponents/src/components/NotifyComponent/NotifyComponent.tsx
@@ -1,5 +1,4 @@
 import './NotifyComponent.scss';
-import React, { Component } from 'react';
 import { FaExclamationTriangle, FaCheckCircle, FaInfoCircle } from 'react-icons/fa';
 
 interface Props {
@@ -10,28 +9,24 @@ interface Props {
   };
 }
 
-class NotifyComponent extends Component<Props> {
-  render() {
-    const { className, notifyMessage } = this.props;
-
-    if (!notifyMessage || !this.props.notifyMessage.text) {
-      return null;
-    }
-
-    return (
-      <div className={`notify notify-${notifyMessage.type} ${className}`}>
-        <i className={`notify__triangle notify-${notifyMessage.text}`}>
-          {notifyMessage.type !== 'error' || <FaExclamationTriangle />}
-          {notifyMessage.type !== 'success' || <FaCheckCircle />}
-          {notifyMessage.type !== 'info' || <FaInfoCircle />}
-          {notifyMessage.type !== 'warning' || <FaExclamationTriangle />}
-        </i>
-        <div className={`notify__text notify-${notifyMessage.type}`}>
-          <span>{notifyMessage.text}</span>
-        </div>
-      </div>
-    );
+const NotifyComponent = ({ className, notifyMessage }: Props) => {
+  if (!notifyMessage || !notifyMessage.text) {
+    return null;
   }
-}
+
+  return (
+    <div className={`notify notify-${notifyMessage.type} ${className}`}>
+      <i className={`notify__triangle notify-${notifyMessage.text}`}>
+        {notifyMessage.type !== 'error' || <FaExclamationTriangle />}
+        {notifyMessage.type !== 'success' || <FaCheckCircle />}
+        {notifyMessage.type !== 'info' || <FaInfoCircle />}
+        {notifyMessage.type !== 'warning' || <FaExclamationTriangle />}
+      </i>
+      <div className={`notify__text notify-${notifyMessage.type}`}>
+        <span>{notifyMessage.text}</span>
+      </div>
+    </div>
+  );
+};
 
 export default NotifyComponent;
