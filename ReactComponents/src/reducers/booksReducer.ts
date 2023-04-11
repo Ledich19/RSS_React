1f462709f7814849342a0d8b3829141fc5bba9fd
Aleksandr <ledich19@gmail.com>
2023-04-11 19:33:11 +0300

    feat: add reducers

--- ReactComponents/src/reducers/booksReducer.ts
+++ ReactComponents/src/reducers/booksReducer.ts
@@ -1,14 +1,45 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { GoogleBook, InfoData } from 'app/types';
 
-const initialState = {};
+type State = {
+  search: string;
+  books: GoogleBook[];
+  formBooks: InfoData[];
+};
+interface Actions {
+  type: string;
+}
+interface SetSearch extends Actions {
+  payload: string;
+}
+interface SetAllBooks extends Actions {
+  payload: GoogleBook[];
+}
+interface AddToFormBook extends Actions {
+  payload: InfoData[];
+}
+
+const initialState: State = {
+  search: '1111111111',
+  books: [],
+  formBooks: [],
+};
 
 const booksSlice = createSlice({
-  name: 'enemy',
+  name: 'book',
   initialState,
   reducers: {
-    addEnemyToBoard(state, actions) {},
+    setSearch(state, actions: SetSearch) {
+      return { ...state, search: actions.payload };
+    },
+    setAllBooks(state, actions: SetAllBooks) {
+      return { ...state, books: actions.payload };
+    },
+    addABookToFormBooks(state, actions: AddToFormBook) {
+      return { ...state, formBooks: state.formBooks.concat(actions.payload) };
+    },
   },
 });
 
-export const { addEnemyToBoard } = booksSlice.actions;
+export const { setSearch, setAllBooks, addABookToFormBooks } = booksSlice.actions;
 export default booksSlice.reducer;
