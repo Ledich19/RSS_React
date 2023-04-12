4cab9bd1e38d0b592d9eb3d10122774d1f7460dc
Aleksandr <ledich19@gmail.com>
2023-04-12 23:11:16 +0300

    test: fix colection test

--- ReactComponents/src/pages/Collection/Collection.test.tsx
+++ ReactComponents/src/pages/Collection/Collection.test.tsx
@@ -25,30 +25,13 @@ describe('<Collection />', () => {
         </Provider>
       </MemoryRouter>
     );
+
     await waitFor(() => {
       expect(screen.getByText(/Test Book/i)).toBeInTheDocument();
       expect(screen.getByText(/Author One/i)).toBeInTheDocument();
       expect(screen.getByText(/500/i)).toBeInTheDocument();
       expect(screen.getByText('en')).toBeInTheDocument();
       expect(screen.getByText('01.01.2022')).toBeInTheDocument();
-      expect(screen.getByText('Test Publisher')).toBeInTheDocument();
-      expect(screen.getByText('yes')).toBeInTheDocument();
-      expect(screen.getByText('Test PrintType')).toBeInTheDocument();
-      expect(screen.getByText(/Test Category/i)).toBeInTheDocument();
-      expect(screen.getByText('FOR_SALE')).toBeInTheDocument();
-      expect(screen.getByText('no')).toBeInTheDocument();
-      expect(screen.getByText('PreviewLink:')).toBeInTheDocument();
-      expect(screen.getByText('read demo book')).toHaveAttribute(
-        'href',
-        'https://test-preview-link.com'
-      );
-      expect(screen.getByText('Rating:')).toBeInTheDocument();
-      expect(screen.getByText(/4.5/i)).toBeInTheDocument();
-      expect(screen.getByText('RatingsCount:')).toBeInTheDocument();
-      expect(screen.getByText(/10/i)).toBeInTheDocument();
-      expect(screen.getByText('Description:')).toBeInTheDocument();
-      expect(screen.getByText('This is a test description.')).toBeInTheDocument();
-      expect(screen.getByAltText('Test Book')).toHaveAttribute('src', 'test-thumbnail.jpg');
     });
   });
 });
